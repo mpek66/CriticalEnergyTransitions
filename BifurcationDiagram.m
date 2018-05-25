15ab46c91fc0a190e9d170bdec98ca4843fb6577
Kaitlin Hill <kaitlin.m.s.hill@gmail.com>
2018-05-25 14:47:08 -0500

    Add bifurcation diagram Matlab file. Add file to plot periodic solutions.

---
+++ BifurcationDiagram.m
@@ -0,0 +1,79 @@
+% This script plots solution trajectories
+
+clear,clc
+
+% Parameters
+r=10;
+w=2*pi;
+% h=10;
+
+hvec = -25:0.1:25;
+
+options = odeset('RelTol',1e-5,'AbsTol',1e-8);
+
+h_bif_diag = [];
+bif_diag = [];
+for n = 1:length(hvec)
+    h = hvec(n);
+    disp(h)
+    % Find periodic solution trajectories %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    x0 = -5:0.1:5;
+    tend = 10;
+
+    % Run for several initial conditions for a long time
+%     [~,x] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(1));
+%     xprev = x(end); 
+%     unstable = 0;
+%     figure, hold on
+    for m = 1:length(x0)
+        [t,x] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(m));
+%         plot(t,x) 
+    
+%         if (abs(xprev - x(end)) > 0.1) && (unstable==0)
+%             x0_unstable = (x0(m) + x0(m-1))/2;
+%             [t,x] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[tend, 0],x0_unstable);
+% %             plot(t,x)
+% 
+%             x0_unstable = x(end);
+%             unstable = 1;
+%         end
+%         xprev = x(end);
+    end
+
+    % Re-run ode45 for first and last x0 to get the initial conditions for the
+    % steady-states
+    [~,xfirst] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(1),options);
+    [~,xlast] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(end),options);
+    
+    % Create one period of the periodic solutions
+    [t1,x1] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, 1],xfirst(end),options);
+    [t2,x2] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, 1],xlast(end),options);
+%     [~,xunstable] =  ode45(@(t,x) h+r*x-x^3+sin(w*t),[1, 0],x0_unstable);
+    
+    % Test plot
+%     figure, hold on
+%     plot(t1,x1)
+%     plot(t2,x2)
+%     clf
+    
+    % Find minima of the periodic solutions
+    min1 = min(x1); 
+    min2 = min(x2);
+%     minun = min(xunstable);
+    
+    % Add minima to bifurcation diagram
+    if min1==min2
+        h_bif_diag = [h_bif_diag, h];
+        bif_diag = [bif_diag, min1];
+    else
+        h_bif_diag = [h_bif_diag, h, h];
+        bif_diag = [bif_diag, min1, min2];
+    end
+end
+
+figure
+scatter(h_bif_diag,bif_diag)
+
+pause(0.01)
+
+
---
+++ PeriodicSolutions.m
@@ -0,0 +1,51 @@
+% This script plots solution trajectories
+
+clear,clc
+
+% Parameters
+r=10;
+w=2*pi;
+h=10;
+
+% Plot solution trajectories %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+x0 = -5:0.1:5;
+tend = 10;
+
+% Run for several initial conditions for a long time
+figure, hold on
+
+xprev = -2; unstable = 0;
+for m = 1:length(x0)
+    [t,x] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(m));
+
+    plot(t,x) 
+    
+    if (abs(xprev - x(end)) > 1) && (unstable==0)
+        x0_unstable = (x0(m) + x0(m-1))/2;
+        
+        [t,x] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[tend, 0],x0_unstable);
+        
+        plot(t,x)
+        
+        x0_unstable = x(end);
+        unstable = 1;
+    end
+    
+    xprev = x(end);
+end
+
+% Re-run ode45 for first and last x0 to get the initial conditions for the
+% steady-states
+[~,xfirst] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(1));
+[~,xlast] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(end));
+[~,xunstable] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, tend],x0(end));
+
+% Create and plot all periodic solutions
+[t1,x1] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, 1],xfirst(end));
+[t2,x2] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, 1],xlast(end));
+[t3,x3] = ode45(@(t,x) h+r*x-x^3+sin(w*t),[0, 1],x0_unstable);
+
+figure, hold on
+plot(t1,x1)
+plot(t2,x2)
+plot(t3,x3)
